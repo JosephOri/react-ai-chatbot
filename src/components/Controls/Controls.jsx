f867e311d08205a928a8f42e377c227a3b7448e3
josephori <ori100no@gmail.com>
2025-05-21 19:37:57 +0300

    Enhance Controls component with loading state and improve accessibility features

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,49 +1,53 @@
-import React, { useState } from 'react';
-import styles from './Controls.module.css';
+import { useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
+import styles from './Controls.module.css';
 
-const Controls = ({ onSend }) => {
+export function Controls({ isDisabled = false, onSend }) {
   const [content, setContent] = useState('');
 
-  const handleContentSend = () => {
-    if (content) {
+  function handleContentChange(event) {
+    setContent(event.target.value);
+  }
+
+  function handleContentSend() {
+    if (content.length > 0) {
       onSend(content);
       setContent('');
     }
-  };
+  }
 
-  const handleEnterPress = (e) => {
-    if (e.key === 'Enter' && !e.shiftKey) {
-      e.preventDefault();
+  function handleEnterPress(event) {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault();
       handleContentSend();
     }
-  };
+  }
 
   return (
     <div className={styles.Controls}>
       <div className={styles.TextAreaContainer}>
         <TextareaAutosize
-          placeholder="Type your message here..."
           className={styles.TextArea}
+          disabled={isDisabled}
+          placeholder="Message AI Chatbot"
           value={content}
-          onChange={(e) => setContent(e.target.value)}
-          onKeyDown={handleEnterPress}
-          maxRows={4}
           minRows={1}
+          maxRows={4}
+          onChange={handleContentChange}
+          onKeyDown={handleEnterPress}
         />
       </div>
-      <button className={styles.Button} onClick={handleContentSend}>
+      <button className={styles.Button} disabled={isDisabled} onClick={handleContentSend}>
         <SendIcon />
       </button>
     </div>
   );
-};
+}
 
-const SendIcon = () => {
+function SendIcon() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#434343">
-      <path d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z" />
+    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
+      <path d="M120-160v-240l320-80-320-80v-240l760 320-760 320Z" />
     </svg>
   );
-};
-export default Controls;
+}
